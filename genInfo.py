8d4e5fea42dc3ad9bc0e555a2ddc8b7451677150
myandroidgames <myandroidgames@163.com>
2015-07-15 10:06:09 +0800

    first demo.

--- genInfo.py
+++ genInfo.py
@@ -11,17 +11,21 @@ SERVER_FILE_BASE_PATH = 'https://github.com/myandroidgames/myandroidgames.github
 ICON_PATH_IN_APK='res/drawable/icon.png'
 
 ICON_SUFFIX='.icon.png'
+JSON_SUFFIX='.json'
+VERSION_NAME_PREFIX='versionName='
 
 JSON_KEY_NAME='name'
 JSON_KEY_SIZE='size'
 JSON_KEY_ICON='iconlink'
 JSON_KEY_LINK='downloadlink'
+JSON_KEY_VERSION='version'
 
 '''
 name,
 size,
 downloadlink,
 iconlink,
+version,
 '''
 
 def genIcon(apkin,iconOut):
@@ -35,15 +39,38 @@ def genIcon(apkin,iconOut):
     target.write(data)
     target.close()
 
+def getNameAndVersion(apkPath):
+    p = subprocess.Popen(['./aaptdump.sh',apkPath],stdout=subprocess.PIPE)
+    output = p.communicate()
+    strr =  output[0].split('\n')
+    #parse version
+    #version out put will be : package: name='com.king.candycrushsaga' versionCode='1054002' versionName='1.54.0.2' platformBuildVersionName='4.0.2-238991'
+    version = strr[0]
+    #genJson(apkPath)
+    #remove package: prefix
+    version = version.split(':')[1]
+    versionName = version.split()[2]
+    versionName = versionName[len(VERSION_NAME_PREFIX)+1:-1]
+    #parse name
+    #package name format: application-label:'Candy Crush Saga'
+    name = strr[1]
+    name = name.split(':')[1]
+    #remove start and end '
+    name = name[1:len(name)-1]
+    #print versionName,"|",name
+    return [name,versionName]
 def genJson(apkin):
     data = {}
     path,ext=splitext(apkin)
-    data[JSON_KEY_NAME] =  'test-game'
+    name,version =  getNameAndVersion(apkin)
+    #print name,version
+    data[JSON_KEY_NAME] =  name
+    data[JSON_KEY_VERSION] = version
     data[JSON_KEY_SIZE] =  getsize(apkin)
-    data[JSON_KEY_ICON] =  path+ ICON_SUFFIX
+    data[JSON_KEY_ICON] =  path + ICON_SUFFIX
     data[JSON_KEY_LINK] =  apkin
 
-    outfile=open('testjson.json','w')
+    outfile=open(path+JSON_SUFFIX,'w')
     json.dump(data,outfile,indent=4)
 
 
@@ -59,19 +86,6 @@ else:
 apkPath=argv[1]
 
 
-#genJson(apkPath)
-p = subprocess.Popen(['./aaptdump.sh',apkPath],stdout=subprocess.PIPE)
-output = p.communicate()
-strr =  output[0].split('\n')
-version = strr[0]
-name = strr[1]
-name = name.split(':')[1]
-name = name[1:len(name)-1]
-print version,"|",name
-
-
-exit()
-
 #dirPath, apkName = split(apkPath)
 
 #apkName = apkName[:-len('.apk')]
@@ -81,9 +95,16 @@ exit()
 path,ext=splitext(apkPath)
 #print 'path:',path,"|",ext
 if isfile(path+ICON_SUFFIX):
-    print path+ICON_SUFFIX,'already exist!\n'
+    print path+ICON_SUFFIX,'already exist!'
 else:
     genIcon(apkPath,path)
+    print path + ICON_SUFFIX, " created!"
+
+if isfile(path + JSON_SUFFIX):
+    print path + JSON_SUFFIX, 'already exist!'
+else:
+    genJson(apkPath)
+    print path + JSON_SUFFIX, ' created!'
 
 
 
